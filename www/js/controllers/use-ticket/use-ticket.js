9ce3b27f7faf82cb016ede99db381c841c26d0bc
Tilwani <tilwanit@amazon.com>
2016-11-28 00:53:28 -0800

    v4

--- www/js/controllers/use-ticket/use-ticket.js
+++ www/js/controllers/use-ticket/use-ticket.js
@@ -1,4 +1,24 @@
 angular.module('starter.controllers')
-  .controller('UseTicketCtrl', function ($scope, $stateParams, Tickets, $cordovaBeacon) {
+  .controller('UseTicketCtrl', function ($scope, $rootScope, $stateParams, $ionicPlatform, Tickets, $cordovaBeacon) {
     $scope.ticket = Tickets.get($stateParams.ticketId);
+
+    $scope.beacons = {key: 'value'};
+
+    $ionicPlatform.ready(function () {
+
+      $cordovaBeacon.requestWhenInUseAuthorization();
+
+      $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function (event, pluginResult) {
+        var uniqueBeaconKey;
+        for (var i = 0; i < pluginResult.beacons.length; i++) {
+          uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
+          $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+        }
+        $scope.$apply();
+      });
+
+      $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("estimote", "b9407f30-f5f8-466e-aff9-25556b57fe6d"));
+
+    });
+
   });
