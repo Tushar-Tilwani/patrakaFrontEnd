5306cfd4ee79b47a427e83be746c066bf612eb38
Tilwani <tilwanit@amazon.com>
2016-12-03 14:34:56 -0800

    v4

--- www/js/controllers/use-ticket/use-ticket.js
+++ www/js/controllers/use-ticket/use-ticket.js
@@ -1,14 +1,18 @@
 angular.module('starter.controllers')
   .controller('UseTicketCtrl', function ($scope, $rootScope, $stateParams, $ionicPlatform, Tickets, $cordovaBeacon) {
     $scope.ticket = Tickets.get($stateParams.ticketId);
-
-    $scope.beacons = {key: 'value'};
+    $scope.t = '1a';
+    $scope.beacons = {};
 
     $ionicPlatform.ready(function () {
 
       $cordovaBeacon.requestWhenInUseAuthorization();
 
+      $scope.t += '::';
+
       $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function (event, pluginResult) {
+        $scope.t += pluginResult.beacons.length;
+
         var uniqueBeaconKey;
         for (var i = 0; i < pluginResult.beacons.length; i++) {
           uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
@@ -17,7 +21,11 @@ angular.module('starter.controllers')
         $scope.$apply();
       });
 
-      $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("estimote", "b9407f30-f5f8-466e-aff9-25556b57fe6d"));
+      var f = $cordovaBeacon.createBeaconRegion("estimote", "b9407f30-f5f8-466e-aff9-25556b57fe6d");
+
+      $cordovaBeacon.startRangingBeaconsInRegion(f);
+
+      $cordovaBeacon.startAdvertising(f);
 
     });
 
