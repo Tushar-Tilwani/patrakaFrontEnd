2c0138b4f53b9ed5a370349ec70a215394bfc692
Tilwani <tilwanit@amazon.com>
2017-02-11 16:24:15 -0800

    more features

--- www/js/controllers/common/about-us/about-us.js
+++ www/js/controllers/common/about-us/about-us.js
@@ -1,27 +1,3 @@
 angular.module('starter.controllers')
-  .factory('mySocket', function (socketFactory) {
-    var myIoSocket = io.connect('http://localhost:3000/', {query: 'id=bar'});
-
-    return socketFactory({
-      ioSocket: myIoSocket
-    });
-  })
-  .controller('AboutUsCtrl', function ($scope, mySocket) {
-
-    $scope.settings = {
-      enableFriends: true
-    };
-
-    mySocket.on('connId', function (data) {
-      $scope.text = data;
-    });
-
-    mySocket.on('pass', function (data) {
-      $scope.text = data;
-    });
-
-    $scope.send = function () {
-      mySocket.emit('msg', 'yaya');
-    };
-
+  .controller('AboutUsCtrl', function ($scope, Vendors) {
   });
